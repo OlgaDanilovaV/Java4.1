fbb73b59f225efdb432acdd31ba83aa6e2413013
OlgaDanilovaV <danilovaolga2235@gmail.com>
2020-05-12 14:44:11 +0300

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+            int price = 10000;
+            int coins = 20;
+            int miles = 1;
+            int bonus = price / coins * miles;
+        return bonus;
+    }
+}
