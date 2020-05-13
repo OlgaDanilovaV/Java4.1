7aae9cafc6c35526d50ce986af6dccb61c0341e4
OlgaDanilovaV <danilovaolga2235@gmail.com>
2020-05-13 13:05:54 +0300

    Changed BonusMilesService

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,6 +1,5 @@
 public class BonusMilesService {
     public int calculate(int cost) {
-            int price = 10000;
             int coins = 20;
             int miles = 1;
             int bonus = cost / coins * miles;
